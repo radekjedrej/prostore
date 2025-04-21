1e01514925ac1b712aec6d6072adf053b5f9c925
radekjedrej <radekjedrej@gmail.com>
2025-04-21 11:16:51 +0200

    FEAT: Prisma + Data

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,14 @@
 import ProductList from '@/components/shared/product/product-list';
-import sampleData from '@/db/sample-data'
+import { getLatestProducts } from '@/lib/actions/product.actions';
 
-const HomePage = () => {
-  return ( 
+const HomePage = async () => {
+  const latestProducts = await getLatestProducts();
+
+  return (
     <>
-      <ProductList data={sampleData.products} title="Newest Arrivals" limit={4}/>
+      <ProductList data={latestProducts} title="Newest Arrivals" limit={4} />
     </>
-   );
+  );
 }
- 
+
 export default HomePage;
